91a9a42c89d7b3a5af7df8528119b9e60ea58346
Joker Haha <noting4765@gmail.com>
2018-07-29 19:12:55 +0530

    upload

---
+++ short.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Circle{
+
+public static void main (String[] args){
+
+Scanner scanner =new Scanner(System.in);
+
+doublsn kjvljsdj
+System.out.println("enter the radius:");
+radius= scanner.nextDouble();
+double area=0.0;
+System.out.println("radius entered by user:"+radius);
+
+
+area = Math.PI*radius*radius;
+System.out.println(area);
+}
+
+}
